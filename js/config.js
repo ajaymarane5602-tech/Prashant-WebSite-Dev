9d9191c8a39de99388ff14309a6b18e0af23131a
ajaymarane5602-tech <ajaymarane5602@gmail.com>
2026-03-06 21:27:44 +0530

    Upload js/config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  brandName: 'Vandika Harvest',
+  whatsappNumber: '918669083518',
+  supportEmail: 'hello@vandikaharvest.com',
+  supportPhone: '+918669083518',
+  address: 'Vandika Harvest Foods, Sangli, Maharashtra, India',
+  facebookPageUrl: 'https://www.facebook.com/share/1bQK2vYjuG/'
+};
